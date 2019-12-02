06e8ce8126330975b9a9a7dcab870f1909d2e73d
firdianadi <48267232+firdianadi@users.noreply.github.com>
2019-12-02 16:48:22 +0700

    1.2
    ad ii

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title> {{sc_config('ADMIN_TITLE')}}  | {{ $title??'' }}</title>
+  <title> {{sc_config('ADMIN_TITLE')}}  | {{ $title??'' }} uji coba</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
